11bd554e2ab991d3f9fee88c04658837fc93c40d
Tim McHugh <tim@digitaltim.com>
2017-03-30 13:05:46 -0400

    Introduce query builder
    Use local web server: php artisan serve
    
    https://laracasts.com/series/laravel-from-scratch-2017/episodes/6

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,16 @@
 |
 */
 
-Route::get('/', function () {
-	$tasks = [
-		'Go to the store',
-		'Finish my screencast',
-		'Clean the house'
-	];
-	
-    return view('welcome', compact('tasks'));
+Route::get('/tasks', function () {
+	$tasks = DB::table('tasks')->latest()->get();
+
+    return view('tasks.index', compact('tasks'));
+});
+
+Route::get('/tasks/{task}', function ($id) {
+	$task = DB::table('tasks')->find($id);
+
+    return view('tasks.show', compact('task'));
 });
 
 Route::get('/about', function () {
