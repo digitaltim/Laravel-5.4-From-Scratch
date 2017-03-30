2991efdb3759eb9b95964f2ac75f2a21fcfe048f
Tim McHugh <tim@digitaltim.com>
2017-03-30 11:49:29 -0400

    Introduce passing data to views
    Use local web server: php artisan serve
    
    https://laracasts.com/series/laravel-from-scratch-2017/episodes/5

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,13 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+	$tasks = [
+		'Go to the store',
+		'Finish my screencast',
+		'Clean the house'
+	];
+	
+    return view('welcome', compact('tasks'));
 });
 
 Route::get('/about', function () {
