6980516e696c75668adc2457254402c60d1902b1
Tim McHugh <tim@digitaltim.com>
2017-03-30 15:59:33 -0400

    Introduce controllers
    Use local web server: php artisan serve
    
    https://laracasts.com/series/laravel-from-scratch-2017/episodes/8

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,7 @@
 <?php
 
-use App\Task;
-
-Route::get('/tasks', function () {
-	$tasks = Task::all();
-
-    return view('tasks.index', compact('tasks'));
-});
-
-Route::get('/tasks/{task}', function ($id) {
-	$task = Task::find($id);//DB::table('tasks')->find($id);
-
-    return view('tasks.show', compact('task'));
-});
+Route::get('/tasks', 'TasksController@index');
+Route::get('/tasks/{task}', 'TasksController@show');
 
 Route::get('/about', function () {
     return view('about');
