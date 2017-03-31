32825ed47cd4fa4b2b0c36b6eca2f53168f56b5d
Tim McHugh <tim@digitaltim.com>
2017-03-31 14:54:15 -0400

    Introduce layout structure using blade
    Use local web server: php artisan serve
    
    https://laracasts.com/series/laravel-from-scratch-2017/episodes/10

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,4 @@
 <?php
 
-Route::get('/tasks', 'TasksController@index');
-Route::get('/tasks/{task}', 'TasksController@show');
-
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('/', 'PostsController@index');
+Route::get('/posts/{post}', 'PostsController@show');
